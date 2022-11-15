6ead750d91170b7533de3d24c96e9ecb5ffb17cb
Gl33ch3r <37208041+Gl33ch3r@users.noreply.github.com>
2022-11-15 08:32:05 +0800

    Update sslh-fix-reboot.sh

--- sslh-fix/sslh-fix-reboot.sh
+++ sslh-fix/sslh-fix-reboot.sh
@@ -8,10 +8,13 @@ systemctl stop sslh
 systemctl daemon-reload
 systemctl disable ws-tls
 systemctl disable sslh
+systemctl disable squid
 systemctl daemon-reload
 systemctl enable sslh
+systemctl enable squid
 systemctl enable ws-tls
 systemctl start sslh 
+systemctl start squid
 /etc/init.d/sslh start 
 /etc/init.d/sslh restart 
 systemctl start ws-tls
