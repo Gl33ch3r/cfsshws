b6c02d5d2908a81848a4231f8246644f3312f7f5
Gl33ch3r <37208041+Gl33ch3r@users.noreply.github.com>
2022-11-22 21:29:45 +0800

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -20,7 +20,7 @@ apt -y install chrony
 timedatectl set-ntp true
 systemctl enable chronyd && systemctl restart chronyd
 systemctl enable chrony && systemctl restart chrony
-timedatectl set-timezone Asia/Jakarta
+timedatectl set-timezone Asia/Manila
 chronyc sourcestats -v
 chronyc tracking -v
 mkdir -p /etc/xray
