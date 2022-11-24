e0a3e0f3491b85cc97d59a758d763f54b60f64e1
Gl33ch3r <37208041+Gl33ch3r@users.noreply.github.com>
2022-11-24 10:07:21 +0800

    Add files via upload

--- xray/renewvless.sh
+++ xray/renewvless.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 ALLOWEDIP=$(curl https://raw.githubusercontent.com/Gl33ch3r/cfsshws/main/ipvps.txt | grep $MYIP)
-if [ $MYIP == $ALLOWEDIP ]; then
+if [[ $MYIP == $ALLOWEDIP ]]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
